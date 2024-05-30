13ac05b775b3e4ac1848edc562108b8b8b64709c
Ian Maher <ianm@internal.3j-consulting.com>
2024-05-30 14:52:06 -0700

    testing image links

--- script.js
+++ script.js
@@ -5,16 +5,16 @@
 
  // List of the 10 largest cities in the world with their coordinates, image URLs, and descriptions
  var cities = [
-  { name: 'Nehalem Basin', coordinates: [-123.9303, 45.7195], imageUrl: 'https://yahnjr.github.io/map-portfolio/pictures/nehalem.png', description: 'Efforts to protect and restore Coho Salmon habitat in the Coast Range. Click picture to read more.', title: 'Protecting Coho Salmon in the Nehalem Watershed', zoomLevel: 12 },
-  { name: 'Tallahassee', coordinates: [-84.2807, 30.4383], imageUrl: 'https://yahnjr.github.io/map-portfolio/pictures/ppgis.png', description: 'Designing straight-forward apps for the collection of public input.', title: 'Application Development for Public Input', zoomLevel: 12 },
-  { name: 'Lake Oswego', coordinates: [-122.6706, 45.4207], imageUrl: 'https://yahnjr.github.io/map-portfolio/pictures/lakeo.png', description: 'A look at Oregon white oaks, a keystone species, in the area.', title: 'Oregon White Oaks in Lake Oswego', zoomLevel: 12 },
-  { name: 'PCC Rock Creek', coordinates: [-122.8405, 45.5582], imageUrl: 'https://yahnjr.github.io/map-portfolio/pictures/pccrockcreek.jpg', description: 'A drone mapping workflow from start to finish.', title: 'Drone Mapping of PCC Rock Creek Learning Garden', zoomLevel: 12 },
-  { name: 'Northern Oregon Coast', coordinates: [-123.9625, 45.5231], imageUrl: 'https://yahnjr.github.io/map-portfolio/pictures/northcoast.jpg', description: 'Looking for ways to quantify and reduce wildlife injuries in the Norther Oregon coast. ', title: 'Wildlife Injuries in the North Coast', zoomLevel: 12 },
-  { name: 'Monterey Bay', coordinates: [-121.8947, 36.6002], imageUrl: 'https://yahnjr.github.io/map-portfolio/pictures/monterey.jpg', description: 'Spatial analysis to determine potential dive sites in the Monterey Bay area in central California.', title: 'Potential Dive Sites in the Monterey Bay Area', zoomLevel: 12 },
-  { name: 'Chiricahua Mountains', coordinates: [-109.3425, 31.9452], imageUrl: 'https://yahnjr.github.io/map-portfolio/pictures/chiricahuas.jpg', description: 'A map showcasing the fascinating biodiversity of the Chiricahua Mountains in Arizona.', title: 'Fauntastic Chiricahua Mountains', zoomLevel: 12 },
-  { name: 'Taiwan', coordinates: [120.9605, 23.6978], imageUrl: 'https://yahnjr.github.io/map-portfolio/pictures/taiwan.png', description: 'An analysis of changing temperatures in Taiwan and the role of environmental factors in this change. Presented at GIS in Action 2022. .', title: 'Slowing Down the Inevitable', zoomLevel: 12 },
-  { name: 'Europe', coordinates: [14.5058, 46.0569], imageUrl: 'https://yahnjr.github.io/map-portfolio/pictures/europe.jpg', description: 'A map utilizing data obtained in my work as an Intelligence Analyst. A summary of major security incidents over the first 5 months of 2023.', title: 'European Security Alert Summary', zoomLevel: 12 },
-  { name: 'Global', coordinates: [0, 0], imageUrl: 'https://yahnjr.github.io/map-portfolio/pictures/world.jpg', description: 'Infographic exploring the Wellcome Global Monitor\'s poll data on trust in science and medical institutions.', title: 'Global Trust in Health Infographic', zoomLevel: 2 }
+  { name: 'Nehalem Basin', coordinates: [-123.9303, 45.7195], imageUrl: 'https://yahnjr.github.io/map-portfolio/pictures/nehalem.png', description: 'Efforts to protect and restore Coho Salmon habitat in the Coast Range. Click picture to read more.', title: 'Protecting Coho Salmon in the Nehalem Watershed', zoomLevel: 12, link: 'projects/nehalem' },
+  { name: 'Tallahassee', coordinates: [-84.2807, 30.4383], imageUrl: 'https://yahnjr.github.io/map-portfolio/pictures/ppgis.png', description: 'Designing straight-forward apps for the collection of public input.', title: 'Application Development for Public Input', zoomLevel: 12, link: 'projects/ppgis' },
+  { name: 'Lake Oswego', coordinates: [-122.6706, 45.4207], imageUrl: 'https://yahnjr.github.io/map-portfolio/pictures/lakeo.png', description: 'A look at Oregon white oaks, a keystone species, in the area.', title: 'Oregon White Oaks in Lake Oswego', zoomLevel: 12, link: 'projects/lakeoswego' },
+  { name: 'PCC Rock Creek', coordinates: [-122.8405, 45.5582], imageUrl: 'https://yahnjr.github.io/map-portfolio/pictures/pccrockcreek.jpg', description: 'A drone mapping workflow from start to finish.', title: 'Drone Mapping of PCC Rock Creek Learning Garden', zoomLevel: 12, link: 'projects/pccrockcreek' },
+  { name: 'Northern Oregon Coast', coordinates: [-123.9625, 45.5231], imageUrl: 'https://yahnjr.github.io/map-portfolio/pictures/northcoast.jpg', description: 'Looking for ways to quantify and reduce wildlife injuries in the Norther Oregon coast. ', title: 'Wildlife Injuries in the North Coast', zoomLevel: 12, link: 'projects/northcoast' },
+  { name: 'Monterey Bay', coordinates: [-121.8947, 36.6002], imageUrl: 'https://yahnjr.github.io/map-portfolio/pictures/monterey.jpg', description: 'Spatial analysis to determine potential dive sites in the Monterey Bay area in central California.', title: 'Potential Dive Sites in the Monterey Bay Area', zoomLevel: 12, link: 'projects/monterey' },
+  { name: 'Chiricahua Mountains', coordinates: [-109.3425, 31.9452], imageUrl: 'https://yahnjr.github.io/map-portfolio/pictures/chiricahuas.jpg', description: 'A map showcasing the fascinating biodiversity of the Chiricahua Mountains in Arizona.', title: 'Fauntastic Chiricahua Mountains', zoomLevel: 12, link: 'projects/chiricahuas' },
+  { name: 'Taiwan', coordinates: [120.9605, 23.6978], imageUrl: 'https://yahnjr.github.io/map-portfolio/pictures/taiwan.png', description: 'An analysis of changing temperatures in Taiwan and the role of environmental factors in this change. Presented at GIS in Action 2022. .', title: 'Slowing Down the Inevitable', zoomLevel: 12, link: 'projects/taiwan' },
+  { name: 'Europe', coordinates: [14.5058, 46.0569], imageUrl: 'https://yahnjr.github.io/map-portfolio/pictures/europe.jpg', description: 'A map utilizing data obtained in my work as an Intelligence Analyst. A summary of major security incidents over the first 5 months of 2023.', title: 'European Security Alert Summary', zoomLevel: 12, link: 'projects/europe' },
+  { name: 'Global', coordinates: [0, 0], imageUrl: 'https://yahnjr.github.io/map-portfolio/pictures/world.jpg', description: 'Infographic exploring the Wellcome Global Monitor\'s poll data on trust in science and medical institutions.', title: 'Global Trust in Health Infographic', zoomLevel: 2, link: 'projects/world' }
 ];
 
  // Add city points to the map and create city boxes for Section 1
@@ -33,7 +33,7 @@
      cityBox.className = 'city-box';
      cityBox.innerHTML = `
          <h3>${city.title}</h3>
-         <img src="${city.imageUrl}" alt="${city.name}">
+         <a href = "${city.link}"> <img src="${city.imageUrl}" alt="${city.name}"> </a>
          <p>${city.description}</p>
      `;
      cityBox.addEventListener('click', function() {
