312ac6b451be2654175acfe846e0262deecb21e1
Ian Maher <iformaher@gmail.com>
2024-12-18 17:42:00 -0800

    Fixed up about section, added full screen buttons for map embeds.

--- script.js
+++ script.js
@@ -231,3 +231,32 @@ document.getElementById("globe-button").addEventListener('click', () =>{
   sidebar.scrollLeft = 0;
   window.scrollTo(0, 0);
 });
+
+const profileImages = [
+  "resources/pictures/profile/profile1.jpg",
+  "resources/pictures/profile/profile2.jpg",
+  "resources/pictures/profile/profile3.jpg",
+  "resources/pictures/profile/profile4.jpg",
+  "resources/pictures/profile/profile5.jpg",
+];
+let currentIndex = 0;
+const imageElement = document.getElementById("current-image");
+
+function updateImage() {
+  imageElement.src = profileImages[currentIndex];
+}
+
+function nextImage() {
+  currentIndex = (currentIndex + 1) % profileImages.length;
+  updateImage();
+}
+
+function prevImage() {
+  currentIndex = (currentIndex - 1 + profileImages.length) % profileImages.length;
+  updateImage();
+}
+
+document.getElementById("previous-image-button").addEventListener("click", prevImage);
+document.getElementById("next-image-button").addEventListener("click", nextImage);
+
+setInterval(nextImage, 30000);
